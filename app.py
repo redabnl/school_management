ba9efa33a7a8ad96789b884eba54c819e64fed28
redabnl <reda01benloulid@gmail.com>
2024-02-05 16:37:15 -0500

    changements on app.py and config files

--- app.py
+++ app.py
@@ -1,14 +1,23 @@
 from flask import Flask
-from .database import SessionLocal, Base, engine
+# from .database import SessionLocal, Base, engine
+from flask_sqlalchemy import SQLAlchemy
+from config import DevelopementConfig
 
 from dotenv import load_dotenv
 load_dotenv() 
 
 app = Flask(__name__)
+app.config.from_object(DevelopementConfig)
+
+db = SQLAlchemy(app)
+
+@app.route('/')
+def index():
+    return "welcome to school management app iRead"
 
 # @app.route('/')
 # def hellop():
 #     return "hello , python"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
--- config.py
+++ config.py
@@ -1,1 +1,14 @@
-from sqlalchemy import create_engine
+import os 
+
+class Config(object):
+    SECRERT_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    
+class DevelopementConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEVELOPMENT_DATABASE_URI')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    
+class ProductionConfig(Config):
+    DEBUG = False
+    SQLALCHEMY_DATABASE_URI = os.environ.get('PRODUCTION_DATABASE_URI')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
